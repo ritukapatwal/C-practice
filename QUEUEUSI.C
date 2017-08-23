02904dfc1252cb9f58c57567be7ec963d02e8fd5
shivaylamba <shivaylamba@gmail.com>
2017-08-23 13:59:01 +0530

    Add files via upload

---
+++ QUEUEUSI.C
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<conio.h>
+#include<malloc.h>
+int j,i=0,queue[10],max=10,head=-1,tail=-1;
+
+ void create(){
+	   int num;
+	   char choice;
+       do{
+	printf("enter data item to insert in queue\n");
+	scanf("%d",&num);
+	head=0;
+	tail++;
+	queue[tail]=num;
+	printf("do you want to add more elements in queue\n");
+	scanf("%c",&choice);
+	scanf("%c",&choice);
+	}while(choice=='y');
+ }
+void display(){
+printf("the traversal of queue is:\n");
+for(i=head,j=0;i<=tail;i++,j++){
+   printf("data of node %d is %d",j,queue[i]);
+   printf("\n");
+   }
+   }
+void insert(int data){
+if(tail==max-1){printf("insertion not possible\n");
+ }
+ queue[++tail]=data;
+}
+void delet(){
+
+ printf("\nvalue deleted is:\n");
+ printf("%d",queue[head]);
+ head++;
+	}
+
+
+
+
+
+
+
+
+
+void main(){
+clrscr();
+create();
+display();
+insert(10);
+display();
+delet();
+display();
+
+
+ getch(); }
